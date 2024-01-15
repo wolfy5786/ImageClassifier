406b348a6702b62583d6afc3f1273bda7ddc76ec
Wolf1824 <makhijaniankush24@gmail.com>
2024-01-15 09:31:25 -0500

    Added Extract features

--- src/ImageClassifier.java
+++ src/ImageClassifier.java
@@ -1,2 +1,26 @@
+import java.awt.*;
+
 public class ImageClassifier {
+    public static double [] extractFeatures(Picture picture)
+    {
+        int width = picture.width();
+        int height = picture.height();
+        double [] greyscale = new double[width*height];
+        Color color;
+        int k=0;
+        for(int i=0;i<height;i++)
+        {
+            for(int j=0;j<width;j++)
+            {
+                color=picture.get(j,i);
+                greyscale[k]=color.getRed();
+                k++;
+            }
+        }
+        return greyscale;
+    }
+    public static void main (String args[])
+    {
+
+    }
 }
